b673ced0c6694c1cc5d5448c131932c0cb99849e
franklin-fjp <56534819+franklin-fjp@users.noreply.github.com>
2019-11-17 02:47:57 +0800

    test 9.1

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import * as qs from 'query-string';
 
-export const API_LINK = process.env.NODE_ENV === "development" ? "https://feed-qa.fastjobs.fun/" : "https://feed.fastjobs.sg/";
+export const API_LINK = process.env.NODE_ENV === "development" ? "https://feed-qa.fastjobs.sg/" : "https://feed.fastjobs.sg/";
 
 const API_PARAMETERS = "?c=kioskntuc&key=4173221dfccb31979361d4194f3b2fb7&locale=en&v=1.0";
 
@@ -52,54 +52,54 @@ export const sample_tiles = {
             "kioskid": "1040",
             "jobid": "1022",
             "jobttl": "CASHIER",
-            "bannerurl": "https://www-qa.fastjobs.fun/img/kiosk/jobs/banner/banner-cashier.jpg?t=1573928535",
+            "bannerurl": "https://www.fastjobs.sg/img/kiosk/jobs/banner/banner-cashier.jpg?t=1573928535",
             "desc": "<p style=\"text-align: center; font-size: 35px; margin: 0; padding: 0;\"><strong>Job Responsibilities</strong></p><p style=\"margin: 0; padding: 0;\"><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://www.fastjobs.sg/img/kiosk/jobs/desc/cashier.jpg?t=1552030242\" alt=\"\" width=\"100%\" /></p><p style=\"text-align: center; font-size: 35px; padding: 0;\"><strong>Working Hours</strong> </p><ul style=\"font-size: 26px;width:80%;margin:0 auto; padding-left: 15%;\"><li>6 days work week (including Weekends & Public Holidays)</li><li>Rotating shift<ul style=\"list-style: none; margin:0 auto; padding-left: 5%;\"><li>- Full-Time: 7am–3pm / 3pm–11pm</li><li>- Part-Time: 7am–12pm / 12pm–5pm / 5pm–10pm</li></ul></li><li>Permanent night shift (11pm–7am) position available</li></ul>",
-            "img": "https://www-qa.fastjobs.fun/img/kiosk/jobs/fairprice-cashier.jpg?t=1573928535"
+            "img": "https://www.fastjobs.sg/img/kiosk/jobs/fairprice-cashier.jpg?t=1573928535"
         },
         {
             "kioskjobid": "1021",
             "kioskid": "1040",
             "jobid": "1021",
             "jobttl": "RETAIL ASSISTANT",
-            "bannerurl": "https://www-qa.fastjobs.fun/img/kiosk/jobs/banner/banner-retail-assistant.jpg?t=1573928535",
+            "bannerurl": "https://www.fastjobs.sg/img/kiosk/jobs/banner/banner-retail-assistant.jpg?t=1573928535",
             "desc": "<p style=\"text-align: center; font-size: 35px; margin: 0; padding: 0;\"><strong>Job Responsibilities</strong></p><p style=\"margin: 0; padding: 0;\"><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://www.fastjobs.sg/img/kiosk/jobs/desc/retail-assistant.jpg?t=1552030242\" width=\"100%\" /></p><p style=\"text-align: center; font-size: 35px;\"><strong>Working Hours</strong></p><ul style=\"font-size: 26px;width:80%;margin:0 auto; padding-left: 15%;\"><li>6 days work week (including Weekends & Public Holidays)</li><li>Rotating shift<ul style=\"list-style: none; margin:0 auto; padding-left: 5%;\"><li>- Full-Time: 7am–3pm / 3pm–11pm</li><li>- Part-Time: 7am–12pm / 12pm–5pm / 5pm–10pm</li></ul></li><li>Permanent night shift (11pm–7am) position available</li></ul>",
-            "img": "https://www-qa.fastjobs.fun/img/kiosk/jobs/fairprice-retail-assistance.jpg?t=1573928535"
+            "img": "https://www.fastjobs.sg/img/kiosk/jobs/fairprice-retail-assistance.jpg?t=1573928535"
         },
         {
             "kioskjobid": "1020",
             "kioskid": "1040",
             "jobid": "1020",
             "jobttl": "ONLINE PICKER",
-            "bannerurl": "https://www-qa.fastjobs.fun/img/kiosk/jobs/banner/banner-online-fulfilment-assistant.jpg?t=1573928535",
+            "bannerurl": "https://www.fastjobs.sg/img/kiosk/jobs/banner/banner-online-fulfilment-assistant.jpg?t=1573928535",
             "desc": "<p style=\"text-align: center; font-size: 35px; margin: 0; padding: 0;\"><strong>Job Responsibilities</strong></p><p style=\"margin: 0; padding: 0;\"><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://www.fastjobs.sg/img/kiosk/jobs/desc/online-picker.jpg?t=1552030242\" alt=\"\" width=\"100%\" /></p><p style=\"text-align: center; font-size: 35px;\"><strong>Working Hours</strong></p><ul style=\"font-size: 26px;width:80%;margin:0 auto; padding-left: 15%;\"><li>6 days work week (including Weekends & Public Holidays)</li><li>Rotating shift<ul style=\"list-style: none; margin:0 auto; padding-left: 5%;\"><li>- Full-Time: 7am–3pm / 3pm–11pm</li></ul></li></ul>",
-            "img": "https://www-qa.fastjobs.fun/img/kiosk/jobs/fairprice-online-fulfilment-assistance.jpg?t=1573928535"
+            "img": "https://www.fastjobs.sg/img/kiosk/jobs/fairprice-online-fulfilment-assistance.jpg?t=1573928535"
         },
         {
             "kioskjobid": "1025",
             "kioskid": "1040",
             "jobid": "1025",
             "jobttl": "RETAIL SUPERVISOR",
-            "bannerurl": "https://www-qa.fastjobs.fun/img/kiosk/jobs/banner/banner-retail-supervisor.jpg?t=1573928535",
+            "bannerurl": "https://www.fastjobs.sg/img/kiosk/jobs/banner/banner-retail-supervisor.jpg?t=1573928535",
             "desc": "<p style=\"text-align: center; font-size: 35px; margin: 0; padding: 0;\"><strong>Job Responsibilities</strong></p><p style=\"margin: 0; padding: 0;\"><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://www.fastjobs.sg/img/kiosk/jobs/desc/retail-supervisor.jpg?t=1552030242\" alt=\"\" width=\"100%\" /></p><p style=\"text-align: center; font-size: 35px;\"><strong>Working Hours</strong></p><ul style=\"font-size: 26px;width:80%;margin:0 auto; padding-left: 15%;\"><li>6 days work week (including Weekends & Public Holidays)</li><li>Rotating shift<ul style=\"list-style: none; margin:0 auto; padding-left: 5%;\"><li>- Full-Time: 7am–3pm / 3pm–11pm</li></ul></li><li>Permanent night shift (11pm–7am) position available</li></ul>",
-            "img": "https://www-qa.fastjobs.fun/img/kiosk/jobs/fairprice-retail-supervisor.jpg?t=1573928535"
+            "img": "https://www.fastjobs.sg/img/kiosk/jobs/fairprice-retail-supervisor.jpg?t=1573928535"
         },
         {
             "kioskjobid": "1024",
             "kioskid": "1040",
             "jobid": "1024",
             "jobttl": "STOREKEEPER",
-            "bannerurl": "https://www-qa.fastjobs.fun/img/kiosk/jobs/banner/banner-storekeeper.jpg?t=1573928535",
+            "bannerurl": "https://www.fastjobs.sg/img/kiosk/jobs/banner/banner-storekeeper.jpg?t=1573928535",
             "desc": "<p style=\"text-align: center; font-size: 35px; margin: 0; padding: 0;\"><strong>Job Responsibilities</strong></p><p style=\"margin: 0; padding: 0;\"><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://www.fastjobs.sg/img/kiosk/jobs/desc/storekeeper.jpg?t=1552030242\" alt=\"\" width=\"100%\" /><br /></p><p style=\"text-align: center; font-size: 35px;\"><strong>Working Hours</strong></p><ul style=\"font-size: 26px;width:80%;margin:0 auto; padding-left: 15%;\"><li>6 days work week (including Weekends & Public Holidays)</li></ul>",
-            "img": "https://www-qa.fastjobs.fun/img/kiosk/jobs/fairprice-storekeeper.jpg?t=1573928535"
+            "img": "https://www.fastjobs.sg/img/kiosk/jobs/fairprice-storekeeper.jpg?t=1573928535"
         },
         {
             "kioskjobid": "1023",
             "kioskid": "1040",
             "jobid": "1023",
             "jobttl": "SKILLED CUTTER",
-            "bannerurl": "https://www-qa.fastjobs.fun/img/kiosk/jobs/banner/banner-skilled-cutter.jpg?t=1573928535",
+            "bannerurl": "https://www.fastjobs.sg/img/kiosk/jobs/banner/banner-skilled-cutter.jpg?t=1573928535",
             "desc": "<p style=\"text-align: center; font-size: 35px; margin: 0; padding: 0;\"><strong>Job Responsibilities</strong></p><p style=\"margin: 0; padding: 0;\"><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://www.fastjobs.sg/img/kiosk/jobs/desc/skilled-cutter.jpg?t=1552030242\" alt=\"\" width=\"100%\" /></p><p style=\"text-align: center; font-size: 35px;\"><strong>Working Hours</strong></p><ul style=\"font-size: 26px;width:80%;margin:0 auto; padding-left: 15%;\"><li>6 days work week (including Weekends & Public Holidays)</li><li>Rotating shift<ul style=\"list-style: none; margin:0 auto; padding-left: 5%;\"><li>- Full-Time: 7am–3pm / 3pm–11pm</li><li>- Part-Time: 7am–12pm / 12pm–5pm / 5pm–10pm</li></ul></li></ul>",
-            "img": "https://www-qa.fastjobs.fun/img/kiosk/jobs/fairprice-skilled-cutter.jpg?t=1573928535"
+            "img": "https://www.fastjobs.sg/img/kiosk/jobs/fairprice-skilled-cutter.jpg?t=1573928535"
         }
     ]
     }
